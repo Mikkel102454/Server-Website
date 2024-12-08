82f8c7805a72f7cfa25171a32ddf28891dd7ac5d
Dopelegend <lassetheilgaardandersen@gmail.com>
2024-12-08 15:57:40 +0100

    Loadtemplate function

--- js-files/general.js
+++ js-files/general.js
@@ -1,3 +1,10 @@
 function ChangePage(url){
     window.location = url
 }
+
+function LoadTemplate(idTemplate, idParent){
+    main = document.getElementById(idParent);
+    var template = document.getElementById(idTemplate);
+    var clone = template.content.cloneNode(true);
+    main.appendChild(clone);
+}
