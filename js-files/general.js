631a658ec9cc83bcd83c0b21b7199a08ea75d4b6
Dopelegend <lassetheilgaardandersen@gmail.com>
2024-12-08 16:50:23 +0100

    Made switch

--- js-files/general.js
+++ js-files/general.js
@@ -4,6 +4,7 @@ function ChangePage(url){
 
 function LoadTemplate(idTemplate, idParent){
     main = document.getElementById(idParent);
+    main.innerHTML = "";
     var template = document.getElementById(idTemplate);
     var clone = template.content.cloneNode(true);
     main.appendChild(clone);
