055ba096d5a52c32fef19f4f08861d1dd35b9a6b
Dopelegend <lassetheilgaardandersen@gmail.com>
2024-12-08 15:55:38 +0100

    Changes to settings page

--- js-files/settings-page.js
+++ js-files/settings-page.js
@@ -1,15 +1,16 @@
 var current;
 var secondaryColor;
-
+var main
 onload = function(){
     current = document.getElementById("appearance-li");
     current.classList.add("selected");
     secondaryColor = getComputedStyle(document.querySelector('.dark-mode')).getPropertyValue('--secondary-color');
     current.style.backgroundColor = (secondaryColor);
+    LoadTemplate("appearance-settings-template", "main");
     
 }
 
-function LiSettingsButton(id){
+function LiSettingsButton(id, idTemplate, idParent){
     current.classList.remove("selected");
     current.style.backgroundColor = ("transparent");
     li = document.getElementById(id);
@@ -17,4 +18,11 @@ function LiSettingsButton(id){
     li.classList.add("selected");
     current = li;
 
+}
+
+function LoadTemplate(idTemplate, idParent){
+    main = document.getElementById(idParent);
+    var template = document.getElementById(idTemplate);
+    var clone = template.content.cloneNode(true);
+    main.appendChild(clone);
 }
